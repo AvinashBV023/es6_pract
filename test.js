faa2268e21a635805104530551378063ba54a932
Ashok <ashokb@unifytech.com>
2019-04-14 17:26:56 +0530

    'yo'

---
+++ test.js
@@ -0,0 +1,59 @@
+// // let len;
+// // let bs = (arr,n) =>{
+// //     // console.log(arr.indexOf(n));
+// //     len = Math.floor(arr.length/2);
+// //     // console.log(len);
+
+// //     if(arr[len] == n)
+// //         return len;
+// //     else if(n>arr[len])
+// //         return bs(arr.splice(len,len+1),n);
+// //     else
+// //         return bs(arr.splice(0,len),n);
+         
+// //         // console.log(arr);
+
+// // }
+// // console.log(bs([1,4,6,67,78,79,89,123],89));
+// // let obj={
+// //     name:'avi',
+// //     age:25,
+  
+
+// // };
+// // let nmage=function(){
+// //     console.log(`name is ${this.name} and age is ${this.age}`) ;
+// //     }
+// // obj.nmage();
+// // let ynum = obj.nmage.bind(obj);
+// // ynum();
+// // let x=[3,4,2,1,1,2,2,3,4,6,5,7,5,6];
+// let x=[3,1,1,1,2,3,4,2,3,2,2,3,3];
+// let r;
+// // let y=x.filter((item,index)=> {return x.indexOf(item) === index});
+// // let y=Array.from( new Set (x));
+// let withOut = (x) => {
+//     for(let i=0;i<x.length;i++){
+//         console.log(i);
+//         if(x[i] === x[i+1])
+//         {
+//            r= x.splice(i,1);
+//            console.log(r,'r');
+//         }
+    
+//     console.log(x,'x');}
+// }
+// withOut(x);
+// // console.log(y,x);
+// let y=6;
+function avi(){
+    {
+        // let y=0;
+        // console.log(y);
+
+    }
+    y=1;
+
+    console.log(y);
+}
+avi();
